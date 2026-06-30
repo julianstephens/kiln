d2142457b849fb64ee93e52feb0cbf688832ae48
Julian Stephens <julian@julianstephens.net>
2026-06-30 16:28:26 -0400

    Add model schema domain shim

---
+++ python/kiln/schemas/model/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from .._internal import (
+    ModelConfiguration,
+    ModelError,
+    ModelGeneratePayload,
+    ModelGenerateResult,
+    ModelUsage,
+)
+
+__all__ = [
+    "ModelConfiguration",
+    "ModelError",
+    "ModelGeneratePayload",
+    "ModelGenerateResult",
+    "ModelUsage",
+]
